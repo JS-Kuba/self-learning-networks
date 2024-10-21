f8dcd8d54b5e73566488db8833e480a67568fd36
Jacobi <jakub.sochacki@wp.pl>
2024-10-22 00:40:52 +0200

    Dynamic programming - code cleanup

--- sailor_train.py
+++ sailor_train.py
@@ -11,9 +11,9 @@ number_of_episodes = 1000                   # number of training epizodes (multi
 gamma = 1                                # discount factor
 delta_max = 0.0001                            # threshold for convergence
 
-file_name = 'map_simple.txt'
+# file_name = 'map_simple.txt'
 #file_name = 'map_easy.txt'
-# file_name = 'map_big.txt'
+file_name = 'map_big.txt'
 # file_name = 'map_spiral.txt'
 
 reward_map = sf.load_data(file_name)
@@ -27,41 +27,6 @@ strategy = np.random.randint(low=1,high=5,size=np.shape(reward_map))  # random s
 random_strategy_mean_reward = np.mean(sf.sailor_test(reward_map,strategy,1000))
 sf.draw_strategy(reward_map,strategy,"random_strategy_average_reward_=_" + str(np.round(random_strategy_mean_reward,2)))
 
-def value_iteration(reward_map, gamma, delta_max):
-    V = np.zeros((num_of_rows, num_of_columns))  # Initialize value function V(s)
-    delta = float('inf')  # Set delta to infinity to start the iteration
-    
-    while delta >= delta_max:
-        V_pom = np.copy(V)  # Copy V to V_pom
-        delta = 0  # Reset delta
-        
-        # Iterate over all states in the state space
-        for i in range(num_of_rows):
-            for j in range(num_of_columns):
-                state = (i, j)
-                # Update V(s) based on maximum expected future reward
-                V[state] = max(
-                    sum(p * (reward + gamma * V_pom[new_state])
-                        for (new_state, p), reward in sf.get_transitions(state, action, reward_map))
-                    for action in range(1, 5)
-                )
-                # Calculate the max difference in value for convergence check
-                delta = max(delta, abs(V[state] - V_pom[state]))
-                print(delta - delta_max)
-
-    strategy = np.zeros((num_of_rows, num_of_columns), dtype=int)
-    for i in range(num_of_rows):
-        for j in range(num_of_columns):
-            state = (i, j)
-            strategy[state] = np.argmax([
-                sum(p * (reward + gamma * V[new_state])
-                    for (new_state, p), reward in sf.get_transitions(state, action, reward_map))
-                for action in range(1, 5)
-            ]) + 1
-
-    return strategy
-
-# optimal_strategy = value_iteration(reward_map, gamma=gamma, delta_max=delta_max)
 
 # Dynamic Programming - strategy iteration
 while True:
