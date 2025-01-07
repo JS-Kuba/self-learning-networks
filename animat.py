da9a99501caa7755c2d57df8c98bf50b3e407447
Jacobi <jakub.sochacki@wp.pl>
2025-01-07 22:20:29 +0100

    Amimat policy network

--- animat.py
+++ animat.py
@@ -1,14 +1,3 @@
-import numpy as np
-import pdb
-import animat_fun as afun
-from tqdm import tqdm
-
-import wandb
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
-
-
 # static map from lecture:
 type_of_map = -1          
 obs_size = 3          # size of observable area e.g 3x3
@@ -34,187 +23,180 @@ if_cross = True       # observable area is cross-shaped e.g agent see only verti
 # obs_size = 7
 # if_cross = False
 
+import numpy as np
+import pdb
+import animat_fun as afun
+import sys
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
 
+GAMMA = 0.99
+LEARNING_RATE = 1e-3
 
-# Tworzenie modelu sieci neuronowej
-def create_policy_network(obs_size, action_size, dense_size):
-    model = Sequential([
-        Flatten(input_shape=(obs_size, obs_size)),  # Spłaszczenie obserwacji (3x3 -> 9)
-        Dense(dense_size, activation='relu'),              # Warstwa ukryta
-        Dense(dense_size, activation='relu'),
-        Dense(action_size, activation='softmax')   # Wyjście - prawdopodobieństwa akcji
-    ])
-    return model
+class PolicyNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, learning_rate=1e-3):
+        super(PolicyNetwork, self).__init__()
 
-def train_policy(policy_network, optimizer, observations, actions, rewards, gamma=0.97):
-    # Oblicz skumulowane nagrody (G_t)
-    G = []
-    discounted_sum = 0
-    for r in rewards[::-1]:
-        discounted_sum = r + gamma * discounted_sum
-        G.insert(0, discounted_sum)
-    G = np.array(G)
+        self.num_actions = num_actions
+        # Deeper network with more hidden layers
+        self.network = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size // 2),
+            nn.ReLU(),
+            nn.Linear(hidden_size // 2, num_actions),
+            nn.Softmax(dim=1)
+        )
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
 
-    # Normalizacja nagród dla stabilności
-    G = (G - np.mean(G)) / (np.std(G) + 1e-8)
+    def forward(self, state):
+        return self.network(state)
     
-    # Konwersja list na numpy arrays
-    observations = np.array(observations)
-    actions = np.array(actions)
+    def get_action(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        probs = self.forward(Variable(state))
+        temperature = 1.0
+        adjusted_probs = probs.pow(1/temperature)
+        adjusted_probs = adjusted_probs / adjusted_probs.sum()
+        
+        highest_prob_action = np.random.choice(
+            self.num_actions, 
+            p=np.squeeze(adjusted_probs.detach().numpy())
+        )
+        log_prob = torch.log(probs.squeeze(0)[highest_prob_action])
+        return highest_prob_action, log_prob
 
-    # Gradient policy gradient
-    with tf.GradientTape() as tape:
-        logits = policy_network(observations)
-        neg_log_probs = tf.keras.losses.sparse_categorical_crossentropy(actions, logits, from_logits=False)
-        loss = tf.reduce_mean(neg_log_probs * G)
+def update_policy(policy_network, rewards, log_probs):
+    discounted_rewards = []
+    # compute discounted rewards
+    for t in range(len(rewards)):
+        Gt = 0
+        pw = 0
+        for r in rewards[t:]:
+            Gt = Gt + GAMMA**pw * r
+            pw = pw + 1
+        discounted_rewards.append(Gt)
+        
+    discounted_rewards = torch.tensor(discounted_rewards)
+    discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / (discounted_rewards.std() + 1e-9)
 
-    grads = tape.gradient(loss, policy_network.trainable_variables)
+    policy_gradient = []
+    for log_prob, Gt in zip(log_probs, discounted_rewards):
+        policy_gradient.append(-log_prob * Gt)
+    
+    policy_network.optimizer.zero_grad()
+    policy_gradient = torch.stack(policy_gradient).sum()
+    policy_gradient.backward()
+    policy_network.optimizer.step()
 
-    gradients_magnitude = [tf.norm(grad) for grad in grads]
+def my_action(strategy, observation):
+    action, log_prob = strategy.get_action(observation)
+    return action, log_prob
 
-    optimizer.apply_gradients(zip(grads, policy_network.trainable_variables))
+def animat_train(type_of_map, obs_size=3, if_cross=False, num_episodes=200):
+    possible_actions = 4
+    strategy = PolicyNetwork(obs_size**2, possible_actions, hidden_size=512, learning_rate=1e-3)
 
-    return loss.numpy(), gradients_magnitude
+    gamma = 0.97
+    map = afun.generate_map(type_of_map)
+    num_of_rows, num_of_columns = np.shape(map)
+    numsteps = []
+    avg_numsteps = []
+    all_rewards = []
 
-action_size = 4  # Liczba akcji (góra, dół, lewo, prawo)
-dense_size = 32
-policy_network = create_policy_network(obs_size, action_size, dense_size)
-learning_rate=0.001
-optimizer = tf.keras.optimizers.Adam(learning_rate)
-
-def my_action(policy_network, observation):
-    observation = np.expand_dims(observation, axis=0)  # Dodanie wymiaru batch
-    action_probs = policy_network.predict(observation, verbose=0)[0]  # Przewidywane prawdopodobieństwa
-    action = np.random.choice(len(action_probs), p=action_probs)  # Losowy wybór z softmax
-    return action
-
-def animat_train(type_of_map, obs_size=3, if_cross=False, log=True):
-    gamma = 0.97   # współczynnik dyskontowania
-    number_of_episodes = 300
-    epsilon = 1
-    epsilon_decay = 0.99
-    if log:
-        wandb.init(project="zpd-project", config={
-            "gamma": gamma,
-            "episodes": number_of_episodes,
-            "dense_size": dense_size,
-            "lr": learning_rate
-        })
-
-    observations, actions, rewards = [], [], []  # Bufor na doświadczenia
-
-    for epi in tqdm(range(number_of_episodes)):
-        epsilon = max(0.01, epsilon * epsilon_decay/(epi/1000+1))
-        map = afun.generate_map(type_of_map)  # Generacja mapy
-        num_of_rows, num_of_columns = np.shape(map)
+    for epi in range(num_episodes):
         position = afun.start_position(map)
-        max_num_of_steps = 4 * (num_of_rows + num_of_columns)
-
+        max_num_of_steps = 10000
+        
         if_end = False
         step_number = 0
-        cumulated_gamma = 1
-        sum_of_discounted_rewards = 0
+        log_probs = []
+        rewards = []
 
-        while not if_end:  # Pętla kroków w epizodzie
+        while not if_end:
             step_number += 1
-
-            # Obserwacja i wybór akcji
+            
             observation = afun.observable_region(map, obs_size, position, if_cross)
-
-            if np.random.rand() < epsilon:  # epsilon-greedy
-                action = np.random.choice(action_size)
-            else:
-                action = my_action(policy_network, observation)
-            # Przejście do nowego stanu
+            observation = observation.flatten()
+            action, log_prob = my_action(strategy, observation)
             new_position, reward = afun.transition_and_reward(map, position, action)
-
-            # Zbieranie danych
-            observations.append(observation)
-            actions.append(action)
+            log_probs.append(log_prob)
             rewards.append(reward)
 
-            # Aktualizacja pozycji i sumowanie nagród
-            position = new_position
-            sum_of_discounted_rewards += reward * cumulated_gamma
-            cumulated_gamma *= gamma
-
-            # Sprawdzenie warunków zakończenia
-            if reward > 0 or step_number > max_num_of_steps:
+            if (reward > 0) or (step_number > max_num_of_steps):
                 if_end = True
+                update_policy(strategy, rewards, log_probs)
+                numsteps.append(step_number)
+                avg_numsteps.append(np.mean(numsteps[-10:]))
+                all_rewards.append(np.sum(rewards))
+                
+                if epi % 10 == 0:
+                    sys.stdout.write(
+                        "episode: {}, total reward: {}, average_reward: {}, length: {}\n".format(
+                            epi,
+                            np.round(np.sum(rewards), decimals=3),
+                            np.round(np.mean(all_rewards[-10:]), decimals=3),
+                            step_number
+                        )
+                    )
 
-        loss, gradients_magnitude = train_policy(policy_network, optimizer, observations, actions, rewards)
+            position = new_position
 
-        if log:
-            wandb.log({
-                "total_reward": sum_of_discounted_rewards,
-                "loss": loss,
-                "epslion": epsilon
-            })
+    return strategy
 
-        # Czyszczenie buforów po epizodzie
-        observations, actions, rewards = [], [], []
-
-        print(f"\nEpisode {epi+1}: Total Reward = {sum_of_discounted_rewards:.2f}")
-        if sum_of_discounted_rewards < 0.01 and sum_of_discounted_rewards > -0.01:
-            print("Gradients magnitude: ", gradients_magnitude)
-
-
-    return policy_network  # Zwróć model sieci jako strategię
-
-
-def animat_test(strategy, type_of_map, obs_size = 3, if_cross = False):
-    gamma = 0.97 # can be changed in training and test for the same value
-    number_of_episodes = 10
-
+def animat_test(strategy, type_of_map, obs_size=3, if_cross=False, num_episodes=100):
+    gamma = 0.97
     mean_sum_of_discounted_rewards = 0
+    total_episodes_with_positive_reward = 0
 
-    for epi in range(number_of_episodes):
-        map = afun.generate_map(type_of_map)  # can be generate for more than one episode
+    for epi in range(num_episodes):
+        map = afun.generate_map(type_of_map)
         num_of_rows, num_of_columns = np.shape(map)
         position = afun.start_position(map)
         max_num_of_steps = 4*(num_of_rows + num_of_columns)
         
-        if_end = False 
+        if_end = False
         step_number = 0
         sum_of_discounted_rewards = 0
         cumulated_gamma = 1
         path = []
 
-        while (if_end == False):                           # episode steps loop
+        while not if_end:
             step_number += 1
             
-            # square region observed by agent:
             observation = afun.observable_region(map, obs_size, position, if_cross)
-            #print(str(observation))
-            action = my_action(strategy, observation) 
+            observation = observation.flatten()
+            action, log_prob = my_action(strategy, observation)
 
-            new_position, reward = afun.transition_and_reward(map,position,action)
+            new_position, reward = afun.transition_and_reward(map, position, action)
 
             path.append([*position, action, *new_position, reward])
 
-            if (reward > 0)|(step_number > max_num_of_steps):
+            if (reward > 0) or (step_number > max_num_of_steps):
                 if_end = True
+                if reward > 0:
+                    total_episodes_with_positive_reward += 1
 
             position = new_position
             sum_of_discounted_rewards += reward*cumulated_gamma
             cumulated_gamma *= gamma
 
-        mean_sum_of_discounted_rewards += sum_of_discounted_rewards/number_of_episodes
+        mean_sum_of_discounted_rewards += sum_of_discounted_rewards/num_episodes
 
-        print("episode " + str(epi) + ": steps = " + str(step_number) + " sum_of_rewards = " + str(sum_of_discounted_rewards))
+        print(f"episode {epi}: steps = {step_number} sum_of_rewards = {sum_of_discounted_rewards}")
 
         if epi < 5:
             afun.save_map_and_path(map, path, epi)
             afun.save_text_animation(map, path, epi)
 
-    print("after " + str(number_of_episodes) + " episodes:")
-    print("mean sum of discounted rewards = " + str(mean_sum_of_discounted_rewards))
-    return mean_sum_of_discounted_rewards
-    
-strategy = animat_train(type_of_map, obs_size, if_cross, log=True)
-mean_sum_of_discounted_rewards = animat_test(strategy, type_of_map, obs_size, if_cross)
-wandb.log({
-    "mean_sum_of_discounted_rewards": mean_sum_of_discounted_rewards,
-    })
+    print(f"after {num_episodes} episodes:")
+    print(f"mean sum of discounted rewards = {mean_sum_of_discounted_rewards}")
+    print(f"episodes with positive reward = {total_episodes_with_positive_reward}/{num_episodes}")
 
-wandb.finish()
+strategy = animat_train(type_of_map, obs_size, if_cross, num_episodes=300)
+animat_test(strategy, type_of_map, obs_size, if_cross, num_episodes=100)
+
