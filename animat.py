cb0b6d128d186ea387fdb534e92f947ac6b9ba31
Jacobi <jakub.sochacki@wp.pl>
2025-01-27 08:35:36 +0100

    Animat

--- animat.py
+++ animat.py
@@ -11,9 +11,9 @@ import matplotlib.pyplot as plt
 import random
 
 # static map from lecture:
-type_of_map = -1          
-obs_size = 3          # size of observable area e.g 3x3
-if_cross = True       # observable area is cross-shaped e.g agent see only vertical and horizontal neighbouring cells
+# type_of_map = -1          
+# obs_size = 3          # size of observable area e.g 3x3
+# if_cross = True       # observable area is cross-shaped e.g agent see only vertical and horizontal neighbouring cells
 
 # other static map:
 # type_of_map = -2          
@@ -21,9 +21,9 @@ if_cross = True       # observable area is cross-shaped e.g agent see only verti
 # if_cross = False      # squared observable area 
 
 # # random map: middle:
-# type_of_map = 0          
-# obs_size = 3
-# if_cross = False
+type_of_map = 0          
+obs_size = 3
+if_cross = False
 
 # # random map: middle:
 # type_of_map = 2          
@@ -41,7 +41,7 @@ LEARNING_RATE = 1e-3
 class ActorNetwork(nn.Module):
     def __init__(self, num_inputs, num_actions, hidden_size, learning_rate=1e-3):
         super(ActorNetwork, self).__init__()
-        self.num_actions = num_actions  # Add this line to store the number of actions
+        self.num_actions = num_actions
         self.network = nn.Sequential(
             nn.Linear(num_inputs, hidden_size),
             nn.ReLU(),
@@ -61,7 +61,6 @@ class ActorNetwork(nn.Module):
         action = np.random.choice(self.num_actions, p=probs.detach().numpy().squeeze())
         log_prob = torch.log(probs.squeeze(0)[action])
         return action, log_prob
-
 
 
 class CriticNetwork(nn.Module):
@@ -120,7 +119,7 @@ def animat_train_actor_critic(type_of_map, obs_size=3, if_cross=False, num_episo
 
     for epi in range(num_episodes):
         position = afun.start_position(map)
-        max_num_of_steps = 10000
+        max_num_of_steps = 500
         
         if_end = False
         step_number = 0
@@ -170,7 +169,7 @@ def animat_test(strategy, type_of_map, obs_size=3, if_cross=False, num_episodes=
     mean_sum_of_discounted_rewards = 0
     total_episodes_with_positive_reward = 0
 
-    positive_outcome_episodes = []  # Store episodes with positive outcome as (reward, map, path)
+    positive_outcome_episodes = []
 
     for epi in range(num_episodes):
         map = afun.generate_map(type_of_map)
@@ -209,10 +208,8 @@ def animat_test(strategy, type_of_map, obs_size=3, if_cross=False, num_episodes=
 
         print(f"episode {epi}: steps = {step_number} sum_of_rewards = {sum_of_discounted_rewards}")
 
-    # Randomly select 10 episodes from those with positive outcome
-    selected_episodes = random.sample(positive_outcome_episodes, min(10, len(positive_outcome_episodes)))
+    selected_episodes = random.sample(positive_outcome_episodes, min(5, len(positive_outcome_episodes)))
 
-    # Save the selected episodes
     for rank, (reward, map, path) in enumerate(selected_episodes):
         afun.save_map_and_path(map, path, rank)
         afun.save_text_animation(map, path, rank)
